d7cc720e25e60d2ec71da8e30b2fb87003d71104
Leandro Schachner <leandroschachner@gmail.com>
2021-11-19 12:45:50 -0300

    Primer commit

---
+++ upcoming.js
@@ -0,0 +1,57 @@
+let pagina = 1;
+const btnAnterior = document.getElementById("btnAnterior");
+const btnSiguiente = document.getElementById("btnSiguiente");
+
+btnSiguiente.addEventListener("click", () => {
+    if(pagina < 1000){
+    pagina += 1;
+    cargarPeliculas();
+    }
+});
+btnAnterior.addEventListener("click", () => {
+    if(pagina > 1){
+    pagina -= 1;
+    cargarPeliculas();
+    }
+});
+
+const cargarPeliculas = async () => {
+
+    try {
+        const  respuesta = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=b8f62f889387d08df32348acc7386c2a&language=es-MX&page=${pagina}`);
+    
+        //console.log(respuesta);
+
+        //comprobar respuesta correcta
+        if(respuesta.status === 200){
+            const datos = await respuesta.json();
+
+
+            let peliculas = "";
+            datos.results.forEach(pelicula => {
+                peliculas +=`
+                    <div class="pelicula">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                    <h3 class="titulo">${pelicula.title}</h3>
+                    <h2 class=""></h2>
+                    </div>
+                `;
+
+            });
+            document.getElementById("contenedor").innerHTML = peliculas;
+
+        }   else if(respuesta.status === 401){
+               // console.log("Pusiste mal la llave");
+        }   else if(respuesta.status === 404){
+                //console.log("La pelicula que buscas no existe");
+        }   else {
+               // console.log("Hubo un error que no entendemos");
+        }
+
+    } catch(error){
+       // console.log(error);
+    }
+    
+}
+
+cargarPeliculas();
