cb143f33fe801cfa312967e39de99f15cf65c9a4
Leandro <leandroschachner@gmail.com>
2022-12-21 12:14:33 -0300

    Segundo arreglo

--- js/populars.js
+++ js/populars.js
@@ -33,7 +33,7 @@ const cargarPeliculas = async () => {
                     <div class="pelicula">
                         <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
                     <h3 class="titulo">${pelicula.title}</h3>
-                    
+                    <h2 class=""></h2>
                     </div>
                 `;
 
